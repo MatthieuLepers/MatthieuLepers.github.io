7df69ae209ee828e6b7df8e2639e0f52793da928
Aire Ayquaza <aireayquaza@hotmail.fr>
2016-01-15 20:13:51 +0100

    alpha mobile compatibility

--- script/Main.js
+++ script/Main.js
@@ -345,6 +345,11 @@ function setupEvent()
 	}
 	
 	getIp();
+	
+	var agent = navigator.userAgent.toLowerCase();
+	
+	if (agent.contains('android') || agent.contains('iphone') || agent.contains('tablet'))
+		new TactilePad();
 }
 
 window.addEventListener('load', setupEvent);
